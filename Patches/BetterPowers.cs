9c198f0ba47f63add1783fe1b1d6d17d2ed80c9f
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-05-29 04:59:11 +0300

    Add patches to improve powers.

---
+++ Patches/BetterPowers.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaValheimHacks.Patches
+{
+    internal class BetterPowers
+    {
+        [HarmonyPatch(typeof(Player), "StartGuardianPower")]
+        internal class CanEatAgain
+        {
+            private const string Prefix = "Player.StartGuardianPower";
+
+            public static float m_guardianPowerCooldown = 0f;
+
+            [HarmonyPrefix]
+            public static void NoPowerCooldown(Player __instance, ref float ___m_guardianPowerCooldown)
+            {
+                if (!GlobalState.EnableHacks)
+                    return;
+
+                m_guardianPowerCooldown = ___m_guardianPowerCooldown;
+                ___m_guardianPowerCooldown = 0f;
+            }
+
+            [HarmonyPostfix]
+            public static void RevertPowerCooldown(Player __instance, ref float ___m_guardianPowerCooldown)
+            {
+                if (!GlobalState.EnableHacks)
+                    return;
+
+                ___m_guardianPowerCooldown = m_guardianPowerCooldown;
+            }
+        }
+    }
+}
