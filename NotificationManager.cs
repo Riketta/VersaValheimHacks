97e897783e6e67c7a4ad3774505acb6db824b1eb
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-05-31 19:58:59 +0300

    Add NotificationManager.

---
+++ NotificationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VersaValheimHacks
+{
+    internal class NotificationManager
+    {
+        public static bool Notification(string message, MessageHud.MessageType messageType = MessageHud.MessageType.Center)
+        {
+            if (GlobalState.Player is null)
+                return false;
+
+            GlobalState.Player.Message(messageType, message);
+            
+            return true;
+        }
+    }
+}
