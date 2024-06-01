bf8641b65dc1b41fa8e76523ffaf5fb418d0433d
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-06-01 13:47:00 +0300

    Refactor.

--- Patches/PlantsSnapPoints.cs
+++ Patches/PlantsSnapPoints.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using static MeleeWeaponTrail;
+using UnityEngine.Playables;
 
 namespace VersaValheimHacks.Patches
 {
