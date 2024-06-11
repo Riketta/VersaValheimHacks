feeabd02834657ea67f4203dd26b3dad5f0ac6eb
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-06-11 06:19:28 +0300

    Add option to disable Mistlands mist.

--- Options/GodModeOptions.cs
+++ Options/GodModeOptions.cs
@@ -11,5 +11,6 @@ namespace VersaValheimHacks.Options
         public bool FreeCraftingEnabled { get; set; } = true;
         public bool NeverEncumbered { get; set; } = true;
         public float CarryWeightMultiplier { get; set; } = 5f;
+        public bool DisableMistlandsMist { get; set; } = true;
     }
 }
---
+++ Patches/NoMist.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaValheimHacks.Patches
+{
+    internal class NoMist
+    {
+        [HarmonyPatch(typeof(ParticleMist), "Update")]
+        internal class ParticleMistUpdate
+        {
+            private const string Prefix = "ParticleMist.Update";
+
+            [HarmonyPrefix]
+            public static bool DisableMist()
+            {
+                if (!GlobalState.ToggleHacks || !GlobalState.Config.GodModeOptions.DisableMistlandsMist)
+                    return true;
+
+                return false;
+            }
+        }
+    }
+}
