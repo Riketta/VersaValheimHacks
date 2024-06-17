afe4a4ccf107175cd72f925134cb838678b16e93
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-06-17 20:34:48 +0300

    Add ValheimUtils.

---
+++ ValheimUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VersaValheimHacks
+{
+    internal class ValheimUtils
+    {
+        public static bool IsObjectPlayer(GameObject gameObject)
+        {
+            Player player = gameObject.GetComponent<Player>();
+
+            if (player is null)
+                return false;
+
+            string unitName = player.GetPlayerName();
+            string playerName = GlobalState.Player?.GetPlayerName();
+
+            return !string.IsNullOrEmpty(playerName) && playerName == unitName;
+        }
+
+        public static Component[] GetAllComponents(GameObject gameObject)
+        {
+            Component[] components = gameObject.GetComponents(typeof(Component));
+            
+            return components;
+        }
+    }
+}
